d8d928389ff67b2249707ac4e16dd48c1929463e
CasteroApps <casteroapps@gmail.com>
2016-10-09 15:34:17 +0100

    Github Landing Page
    Coded Cloudz Github

---
+++ files/coded.js
@@ -0,0 +1,30 @@
+﻿/*
+ ***************************************
+*		 * CODED FRAMEWORK *  			*
+*	DEVELOPED BY: CASTERO - KUMASI		*
+*										*
+*			  AKA: CODED				*
+ ***************************************
+*/
+
+//appversion
+ function appversion (){
+	 version = "1.0";
+	 return version;
+ }
+
+//showhelpInfo
+ function showhelpInfo () {
+     document.getElementById("help").style.display = "block";
+window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+};
+
+//moreAppslocal
+ function moreApps () {
+    window.location.assign("market://search?q=pub:Castero Apps");
+};
+
+//moreAppsonline
+ function moreAppsonline () {
+	document.getElementById("moreappsonline").click();
+};
