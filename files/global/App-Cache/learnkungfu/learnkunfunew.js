0bdb7a0647ed9c5658d2cc158c99f0fe48ca695c
CodedCloudz <CodedCloudz@gmail.com>
2017-02-05 08:15:34 +0000

    Update learnkunfunew.js

--- files/global/App-Cache/learnkungfu/learnkunfunew.js
+++ files/global/App-Cache/learnkungfu/learnkunfunew.js
@@ -7,4 +7,57 @@
  ***************************************
 */
 
-var _0x3d86=["\x59\x6F\x75\x72\x20\x41\x70\x70\x20\x69\x73\x20\x74\x6F\x6F\x20\x6F\x6C\x64\x2C\x20\x0A\x43\x6C\x69\x63\x6B\x20\x4F\x4B\x20\x74\x6F\x20\x55\x70\x64\x61\x74\x65\x20\x6E\x6F\x77\x21","\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x6D\x61\x72\x6B\x65\x74\x3A\x2F\x2F\x64\x65\x74\x61\x69\x6C\x73\x3F\x69\x64\x3D\x63\x6F\x6D\x2E\x63\x61\x73\x74\x65\x72\x6F\x61\x70\x70\x73\x2E\x6C\x65\x61\x72\x6E\x6B\x75\x6E\x67\x66\x75\x6E\x65\x77","\x63\x6F\x6E\x6E\x65\x63\x74\x65\x64","\x74\x65\x78\x74","\x54\x49\x54\x4C\x45","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x47\x48\x20\x53\x54\x41\x52\x53"];var _0xb14a=[_0x3d86[0],_0x3d86[1],_0x3d86[2],_0x3d86[3],_0x3d86[4],_0x3d86[5],_0x3d86[6],_0x3d86[7],_0x3d86[8]];var _0xf153=[_0xb14a[0],_0xb14a[1],_0xb14a[2],_0xb14a[3],_0xb14a[4],_0xb14a[5],_0xb14a[6],_0xb14a[7],_0xb14a[8]];var _0xc1b3=[_0xf153[0],_0xf153[1],_0xf153[2],_0xf153[3],_0xf153[4],_0xf153[5],_0xf153[6],_0xf153[7],_0xf153[8]];var _0xf13a=[_0xc1b3[0],_0xc1b3[1],_0xc1b3[2],_0xc1b3[3],_0xc1b3[4],_0xc1b3[5],_0xc1b3[6],_0xc1b3[7],_0xc1b3[8]];checkversion();notice();function checkversion(){if(appversion()< 1.0){if(confirm(_0xf13a[0])=== true){window[_0xf13a[2]][_0xf13a[1]]= _0xf13a[3]}}}var codedcloud=_0xf13a[4];function notice(){var _0xdba6x8=document[_0xf13a[7]](_0xf13a[6])[0][_0xf13a[5]];if(_0xdba6x8=== _0xf13a[8]){}}
+//                -- PRELOADERS --
+
+//updateChecker
+      checkversion ();
+
+//Patch
+      patch ();
+
+//Notice
+      notice ();
+
+		
+//checkversion
+ function checkversion () {
+
+if (appversion () < 1.0) { 
+	if (confirm("Your App is too old, \nClick OK to Update now!") === true) {
+		window.location.href = 'market://details?id=com.casteroapps.learnkungfunew';
+	};
+		};
+};
+		
+//codedOnline
+var codedcloud = "connected"
+
+//notice
+ function notice () {
+	//var pagetitle = document.getElementsByTagName("TITLE")[0].text;
+
+if (pagetitle === "GH STARS"){	
+	//alert("The National Service, News, Music, E-payslip & Technical Results function in this App is now fully completed. So tell everyone about this. \n\nAll functions with grayed icon is still under development. \n\nThank you.");
+	//alert("Hello user, The Training Videos function in this App is now completed. Click on the Training Videos button to enjoy your KungFu video lessons.  \nKindly tell all your friends to use Learn KungFu. \n\nAnd also, do the following always to support this app: \n\n1) Click on most of the Ads in this app to support the cost of servers the app is running on. \n\n2) Go to Google Play Store, scroll down and Rate 5stars with a nice comment for this app..");
+	}
+};
+
+//notice
+ function patch () {
+	
+if (pagetitle () === "LEARN KUNGFU" && pagename() === "KUNGFU STANCE"){	
+		
+		//Gallery initialization
+		$("#nanoGallery2").nanoGallery({
+          kind: 'picasa',
+        userID: '102596438567953137786',        
+        album: '6302735542318610097',
+        thumbnailWidth: 'auto',
+		thumbnailHeight: 400,
+		locationHash: false,
+		thumbnailHoverEffect:'borderLighter,imageScaleIn80'
+      });	
+	}
+};
+
+//https://codedcloudz.github.io/files/global/learnkunfunew.js
