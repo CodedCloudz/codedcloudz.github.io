0a3c31484989fefc554bce2975d3c051d089ee51
CodedCloudz <CodedCloudz@gmail.com>
2017-02-06 05:30:56 +0000

    Update kungfuvideos-rescue.js

--- files/global/App-Cache/kungfuvideos/kungfuvideos-rescue.js
+++ files/global/App-Cache/kungfuvideos/kungfuvideos-rescue.js
@@ -40,31 +40,12 @@ if (pagetitle === "GH STARS"){
 	}
 };
 
-$(".youmax").youmax({
-                apiKey:"AIzaSyAlhAqP5RS7Gxwg_0r_rh9jOv_5WfaJgXw",
 
-                channelLink:"https://www.youtube.com/user/shemmati10yt",
-                playlistLink:"https://www.youtube.com/?listType=user_uploads&list=shemmati10yt",
-                
-                defaultTab:"Uploads",         //Uploads|Playlists|Featured
-                videoDisplayMode:"popup",       //popup|link|inline
+//vimeovideos
+function vimeovideos (){
+	if (pagename() === "VIDEOS"){
+		window.location.href = 'http://tiny.cc/77rtiy';
+	}
+};
 
-                maxResults:"20",
-                autoPlay:true,
-                displayFirstVideoOnLoad:true,       //for inline video display mode only
-                
-
-                responsiveBreakpoints   :[600,900,2000,2500],
-
-                loadMoreText            :"<i class=\"fa fa-plus\"></i>&nbsp;&nbsp;Show me more videos..",
-                previousButtonText      :"<i class=\"fa fa-angle-left\"></i>&nbsp;&nbsp;Previous",
-                nextButtonText          :"Next&nbsp;&nbsp;<i class=\"fa fa-angle-right\"></i>",
-                loadingText             :"loading...",
-                allDoneText             :"<i class=\"fa fa-times\"></i>&nbsp;&nbsp;All done..",
-
-                hideHeader              :false,
-                hideTabs                :false,
-                hideLoadingMechanism    :false,
-
-
-            });
+vimeovideos ();
