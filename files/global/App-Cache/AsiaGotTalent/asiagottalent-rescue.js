09633f9087e897d5a0e31514a77d064261329880
CodedCloudz <CodedCloudz@gmail.com>
2017-02-07 06:13:03 +0000

    Update asiagottalent-rescue.js

--- files/global/App-Cache/AsiaGotTalent/asiagottalent-rescue.js
+++ files/global/App-Cache/AsiaGotTalent/asiagottalent-rescue.js
@@ -38,7 +38,7 @@ if (pagetitle === "GH STARS"){
 	//alert("The National Service, News, Music, E-payslip & Technical Results function in this App is now fully completed. So tell everyone about this. \n\nAll functions with grayed icon is still under development. \n\nThank you.");
 	//alert("Hello user, The Training Videos function in this App is now completed. Click on the Training Videos button to enjoy your KungFu video lessons.  \nKindly tell all your friends to use Learn KungFu. \n\nAnd also, do the following always to support this app: \n\n1) Click on most of the Ads in this app to support the cost of servers the app is running on. \n\n2) Go to Google Play Store, scroll down and Rate 5stars with a nice comment for this app..");
 	}
-};
+}; 
 
 $(".youmax").youmax({
                 apiKey:"AIzaSyAlhAqP5RS7Gxwg_0r_rh9jOv_5WfaJgXw",
