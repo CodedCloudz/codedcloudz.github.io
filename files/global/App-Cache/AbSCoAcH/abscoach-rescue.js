869706bf192c88390e8b74fcca55c55d9125a40a
CodedCloudz <CodedCloudz@gmail.com>
2017-02-26 23:34:47 +0000

    Update abscoach-rescue.js

--- files/global/App-Cache/AbSCoAcH/abscoach-rescue.js
+++ files/global/App-Cache/AbSCoAcH/abscoach-rescue.js
@@ -48,7 +48,7 @@ if (pagename() === "GALLERY"){
 		$("#nanoGallery2").nanoGallery({
           kind: 'picasa',
         userID: '107176243627978544958',        
-        album: 'UD3dxoGZcFunJMUv8',
+        album: '6297954975177698881',
        // userID: '102596438567953137786',        
       //  album: '6302735542318610097',
         thumbnailWidth: 'auto',
