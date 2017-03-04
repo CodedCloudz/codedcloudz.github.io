a44693c323a3b32005bdbb0e73e7fe58ce4092d7
CodedCloudz <CodedCloudz@gmail.com>
2017-03-04 04:36:42 +0000

    Update kungfuvideos-rescue.js

--- files/global/App-Cache/kungfuvideos/kungfuvideos-rescue.js
+++ files/global/App-Cache/kungfuvideos/kungfuvideos-rescue.js
@@ -39,34 +39,11 @@ if (pagetitle === "GH STARS"){
 	//alert("Hello user, The Training Videos function in this App is now completed. Click on the Training Videos button to enjoy your KungFu video lessons.  \nKindly tell all your friends to use Learn KungFu. \n\nAnd also, do the following always to support this app: \n\n1) Click on most of the Ads in this app to support the cost of servers the app is running on. \n\n2) Go to Google Play Store, scroll down and Rate 5stars with a nice comment for this app..");
 	}
 };
-		document.getElementsByClassName('yl-loader')[0].innerHTML = "Please Wait.. <br> Videos are loading..";
 
 $(".youmax").youmax({
-                apiKey:"AIzaSyAlhAqP5RS7Gxwg_0r_rh9jOv_5WfaJgXw",
-                	//apiKey:"AIzaSyC3lXNm8-OC5y3uFLUQjPkTCTP6qR7Bv0s", //CASTERO APPS LEARN KUNGFU [ NEW ]
-                channelLink:"https://www.youtube.com/user/shemmati10yt",
-                playlistLink:"https://www.youtube.com/?listType=user_uploads&list=shemmati10yt",
-                
-                defaultTab:"Uploads",         //Uploads|Playlists|Featured
-                videoDisplayMode:"popup",       //popup|link|inline
-
-                maxResults:"20",
-                autoPlay:true,
-                displayFirstVideoOnLoad:true,       //for inline video display mode only
-                
-
-                responsiveBreakpoints   :[600,900,2000,2500],
-
-                loadMoreText            :"<i class=\"fa fa-plus\"></i>&nbsp;&nbsp;Show me more videos..",
-                previousButtonText      :"<i class=\"fa fa-angle-left\"></i>&nbsp;&nbsp;Previous",
-                nextButtonText          :"Next&nbsp;&nbsp;<i class=\"fa fa-angle-right\"></i>",
-                loadingText             :"Please Wait.. <br> Videos are loading..",
-                allDoneText             :"<i class=\"fa fa-times\"></i>&nbsp;&nbsp;All done..",
-
-                hideHeader              :false,
-                hideTabs                :false,
-                hideLoadingMechanism    :false,
+               
 
             });
 
+		document.getElementsByClassName('yl-loader')[0].innerHTML = "Please Wait.. <br> Videos are loading..";
 
