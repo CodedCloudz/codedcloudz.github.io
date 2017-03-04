f1b7af815fd993f7c85bf18d4d235be7128f11b3
CodedCloudz <CodedCloudz@gmail.com>
2017-03-04 15:41:02 +0000

    Update drivingvideos-rescue.js

--- files/global/App-Cache/DrIViNgVIdEoS/drivingvideos-rescue.js
+++ files/global/App-Cache/DrIViNgVIdEoS/drivingvideos-rescue.js
@@ -40,7 +40,7 @@ if (pagetitle === "GH STARS"){
 	}
 };
 
-if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "CHANNEL 3" || pagename() === "SOCIAL MEDIA"){
+if (pagename() === "CHANNEL 1"){
   $(".youmax").youmax({
                 apiKey:"AIzaSyAlhAqP5RS7Gxwg_0r_rh9jOv_5WfaJgXw",
                 	//apiKey:"AIzaSyC3lXNm8-OC5y3uFLUQjPkTCTP6qR7Bv0s", //CASTERO APPS LEARN KUNGFU [ NEW ]
@@ -52,8 +52,7 @@ if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "
 
                 maxResults:"20",
                 autoPlay:true,
-                displayFirstVideoOnLoad:true,       //for inline video display mode only
-                
+                displayFirstVideoOnLoad:true,       //for inline video display mode only                
 
                 responsiveBreakpoints   :[600,900,2000,2500],
 
@@ -66,7 +65,63 @@ if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "
                 hideHeader              :false,
                 hideTabs                :false,
                 hideLoadingMechanism    :false,
-
             });
+	}
 
+
+if (pagename() === "CHANNEL 2"){
+  $(".youmax").youmax({
+                apiKey:"AIzaSyAlhAqP5RS7Gxwg_0r_rh9jOv_5WfaJgXw",
+                	//apiKey:"AIzaSyC3lXNm8-OC5y3uFLUQjPkTCTP6qR7Bv0s", //CASTERO APPS LEARN KUNGFU [ NEW ]
+                channelLink:"https://www.youtube.com/user/shemmati10yt",
+                playlistLink:"https://www.youtube.com/?listType=user_uploads&list=shemmati10yt",
+                
+                defaultTab:"Uploads",         //Uploads|Playlists|Featured
+                videoDisplayMode:"popup",       //popup|link|inline
+
+                maxResults:"20",
+                autoPlay:true,
+                displayFirstVideoOnLoad:true,       //for inline video display mode only                
+
+                responsiveBreakpoints   :[600,900,2000,2500],
+
+                loadMoreText            :"<i class=\"fa fa-plus\"></i>&nbsp;&nbsp;Show me more videos..",
+                previousButtonText      :"<i class=\"fa fa-angle-left\"></i>&nbsp;&nbsp;Previous",
+                nextButtonText          :"Next&nbsp;&nbsp;<i class=\"fa fa-angle-right\"></i>",
+                loadingText             :"Please Wait.. <br> Videos are loading..",
+                allDoneText             :"<i class=\"fa fa-times\"></i>&nbsp;&nbsp;All done..",
+
+                hideHeader              :false,
+                hideTabs                :false,
+                hideLoadingMechanism    :false,
+            });
+	}
+
+
+if (pagename() === "CHANNEL 3"){
+  $(".youmax").youmax({
+                apiKey:"AIzaSyAlhAqP5RS7Gxwg_0r_rh9jOv_5WfaJgXw",
+                	//apiKey:"AIzaSyC3lXNm8-OC5y3uFLUQjPkTCTP6qR7Bv0s", //CASTERO APPS LEARN KUNGFU [ NEW ]
+                channelLink:"https://www.youtube.com/user/shemmati10yt",
+                playlistLink:"https://www.youtube.com/?listType=user_uploads&list=shemmati10yt",
+                
+                defaultTab:"Uploads",         //Uploads|Playlists|Featured
+                videoDisplayMode:"popup",       //popup|link|inline
+
+                maxResults:"20",
+                autoPlay:true,
+                displayFirstVideoOnLoad:true,       //for inline video display mode only                
+
+                responsiveBreakpoints   :[600,900,2000,2500],
+
+                loadMoreText            :"<i class=\"fa fa-plus\"></i>&nbsp;&nbsp;Show me more videos..",
+                previousButtonText      :"<i class=\"fa fa-angle-left\"></i>&nbsp;&nbsp;Previous",
+                nextButtonText          :"Next&nbsp;&nbsp;<i class=\"fa fa-angle-right\"></i>",
+                loadingText             :"Please Wait.. <br> Videos are loading..",
+                allDoneText             :"<i class=\"fa fa-times\"></i>&nbsp;&nbsp;All done..",
+
+                hideHeader              :false,
+                hideTabs                :false,
+                hideLoadingMechanism    :false,
+            });
 	}
