4fbe1600bda87c43753753436aa059beeaec3b75
CodedCloudz <CodedCloudz@gmail.com>
2017-03-07 05:34:08 +0000

    Update makeupvideos-rescue.js

--- files/global/App-Cache/MaKEupVIdEoS/makeupvideos-rescue.js
+++ files/global/App-Cache/MaKEupVIdEoS/makeupvideos-rescue.js
@@ -1,5 +1,5 @@
 //CHANNEL
 if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "CHANNEL 3" || pagename() === "CHANNEL 4" || pagename() === "CHANNEL 5" || pagename() === "CHANNEL 6"){
    
-   window.location.href = 'http://tiny.cc/i9nnjy;
+   window.location.href = 'http://tiny.cc/i9nnjy';
 }
