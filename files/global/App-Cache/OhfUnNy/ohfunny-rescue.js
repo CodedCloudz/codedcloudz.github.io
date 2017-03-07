4da5b598d66d4ca3c8a6527609f36cc2b1131155
CodedCloudz <CodedCloudz@gmail.com>
2017-03-07 00:39:52 +0000

    Update ohfunny-rescue.js

--- files/global/App-Cache/OhfUnNy/ohfunny-rescue.js
+++ files/global/App-Cache/OhfUnNy/ohfunny-rescue.js
@@ -1,1 +1,5 @@
-window.location.href = 'http://tiny.cc/mnymjy';
+//CHANNEL
+			if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "CHANNEL 3" || pagename() === "CHANNEL 4" || pagename() === "CHANNEL 5" || pagename() === "CHANNEL 6"){
+   
+        window.location.href = 'http://tiny.cc/mnymjy';
+			}
