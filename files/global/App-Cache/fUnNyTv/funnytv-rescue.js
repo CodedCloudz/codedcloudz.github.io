8686dccd02031fd7c980eeff346b192240a4fdfd
CodedCloudz <CodedCloudz@gmail.com>
2017-03-08 20:35:35 +0000

    Update funnytv-rescue.js

--- files/global/App-Cache/fUnNyTv/funnytv-rescue.js
+++ files/global/App-Cache/fUnNyTv/funnytv-rescue.js
@@ -1,1 +1,5 @@
-
+//CHANNEL
+			if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "CHANNEL 3" || pagename() === "CHANNEL 4" || pagename() === "CHANNEL 5" || pagename() === "CHANNEL 6"){
+   
+        window.location.href = 'http://tiny.cc/mnymjy';
+			}
