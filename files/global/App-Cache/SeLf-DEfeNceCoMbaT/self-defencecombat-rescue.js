e28d58655a3f9fca31e919f4ab9936da438e1331
CodedCloudz <CodedCloudz@gmail.com>
2017-05-20 22:06:32 +0100

    Update self-defencecombat-rescue.js

--- files/global/App-Cache/SeLf-DEfeNceCoMbaT/self-defencecombat-rescue.js
+++ files/global/App-Cache/SeLf-DEfeNceCoMbaT/self-defencecombat-rescue.js
@@ -32,10 +32,10 @@ if (appversion () < 2.0) {
 
 //checkVersionLable
 var versionLable1 = $("#aboutDialog > h3>a")[0].innerHTML || $("#aboutDialog > h4>a")[0].innerHTML;
+var labelResults = versionLable1.toString();
 
  function checkVersionLable () {
-
-if (versionLable1 !== "SELF-DEFENSE COMBAT 2.0") { 
+if (labelResults !== "SELF-DEFENSE COMBAT 2.0") { 
 	if (confirm("An important Update Available, \nClick OK to Update this App now!") === true) {
 		window.location.href = 'market://details?id=com.casteroapps.selfdefensecombat';
 	};
