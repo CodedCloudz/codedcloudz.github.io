fd460882c805ff7213dbc0e9b780646222c50210
CodedCloudz <CodedCloudz@gmail.com>
2017-05-22 00:25:48 +0100

    Update getrich-rescue.js

--- files/global/App-Cache/GEtRiCh/getrich-rescue.js
+++ files/global/App-Cache/GEtRiCh/getrich-rescue.js
@@ -50,7 +50,7 @@ if (pagename() === "GALLERY"){
 //CHANNEL
 if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "CHANNEL 3" || pagename() === "CHANNEL 4" || pagename() === "CHANNEL 5" || pagename() === "CHANNEL 6"){
    
-        window.location.href = 'http://vimeo.com/search?q=economics';
+        window.location.href = 'http://vimeo.com/search?q=financial+secrets';
 	}
 
 
