88efa4f24b26dec63a321d20e063118fa03127fb
CodedCloudz <CodedCloudz@gmail.com>
2017-05-21 09:43:26 +0100

    Update self-defencecombat-rescue.js

--- files/global/App-Cache/SeLf-DEfeNceCoMbaT/self-defencecombat-rescue.js
+++ files/global/App-Cache/SeLf-DEfeNceCoMbaT/self-defencecombat-rescue.js
@@ -63,7 +63,7 @@ if (pagename() === "GALLERY"){
 //CHANNEL
 if (pagename() === "CHANNEL 1" || pagename() === "CHANNEL 2" || pagename() === "CHANNEL 3" || pagename() === "CHANNEL 4" || pagename() === "CHANNEL 5" || pagename() === "TRAINING VIDEOS"){
    
-        window.location.href = 'http://vimeo.com/search?q=self+defense';
+       // window.location.href = 'http://vimeo.com/search?q=self+defense';
 	}
 
 
