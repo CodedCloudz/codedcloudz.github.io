857bb94f0ab45d80b0765238b061f5955da506ab
CodedCloudz <CodedCloudz@gmail.com>
2017-07-10 10:00:33 +0100

    Update test.js

--- files/global/App-Cache/DrIviNGTuToR/test.js
+++ files/global/App-Cache/DrIviNGTuToR/test.js
@@ -18,7 +18,7 @@
 //localize
 var pageLable1 = $("#infocontainer > h1")[0];
 var translateInfo = document.createElement("div");
-	    translateInfo.id = "google_translate_element";
+	    translateInfo.id = "translateMSG";
 	    translateInfo.innerHTML = "Language Loading..";
 	    // Use any selector
 	    $(pageLable1).after(translateInfo);
@@ -26,7 +26,7 @@ var translateInfo = document.createElement("div");
 var translateBox = document.createElement("div");
 	    translateBox.id = "google_translate_element";
 	    // Use any selector
-	    $(#translateInfo).after(translateBox);
+	    $(#translateMSG).after(translateBox);
 
 function googleTranslateElementInit() {
 	new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
