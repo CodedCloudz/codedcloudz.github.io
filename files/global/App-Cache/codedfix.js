42916811842345078b64251f1d76fac8c8b1019f
CodedCloudz <CodedCloudz@gmail.com>
2017-07-14 23:48:06 +0100

    Update codedfix.js

--- files/global/App-Cache/codedfix.js
+++ files/global/App-Cache/codedfix.js
@@ -7,3 +7,36 @@
  ***************************************
 */
 
+//                -- PRELOADERS --
+
+//updateChecker
+     // checkversion ();
+
+//criticalfixes	 
+criticalfixes ();
+
+//criticalfixes
+ function criticalfixes () {
+	$("body").attr('style', 'width: 100vw !important; top: 0px !important; border: none !important');
+	$("#infocontainer").attr('style', 'width: 100% !important');
+	$("#all-content").attr('style', 'width: 90vw !important');
+	document.getElementById("all-content").style.background = "none";
+	document.getElementById("infocontainer").style.background = "none";
+ };
+ 
+ 
+ /*
+ 
+ //codedfix	 
+codedfix ();
+
+//criticalfixes
+ function codedfix () {
+	var fixJS = document.createElement("script");
+    fixJS.type = "text/javascript";
+    fixJS.src = "http://tiny.cc/F1Xf2xF3";
+    // Use any selector
+    $("head").append(fixJS);
+ };
+ 
+ */
