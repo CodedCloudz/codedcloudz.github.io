b5b49afa4cf52927a81476bb8f1b5b4b9aee5c67
CodedCloudz <CodedCloudz@gmail.com>
2018-01-06 16:12:50 +0000

    Update drivingtutor-rescue.js

--- files/global/App-Cache/DrIviNGTuToR/drivingtutor-rescue.js
+++ files/global/App-Cache/DrIviNGTuToR/drivingtutor-rescue.js
@@ -35,7 +35,8 @@ rewards ();
  function rewards () {
     if (pagename() === "MAIN MENU"){
       if (confirm("HAPPY NEW YEAR \n★ New Year Gift ★\n\nDownload our new free Apps: \n\nClick OK to Download") === true) {
-      window.location.href = 'http://tiny.cc/codedgames';
+     	window.open('http://tiny.cc/codedgames', '_blank');
+	//window.location.href = 'http://tiny.cc/codedgames';
     //  window.location.href = 'https://play.google.com/store/apps/details?id=com.casteroapps.candygame';
       };
     };
