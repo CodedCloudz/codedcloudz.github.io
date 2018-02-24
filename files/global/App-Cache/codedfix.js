0fb20c69aaeb6c6db8e03dfd021c07f8af2977c3
CodedCloudz <CodedCloudz@gmail.com>
2018-02-24 13:27:24 +0000

    Update codedfix.js

--- files/global/App-Cache/codedfix.js
+++ files/global/App-Cache/codedfix.js
@@ -6,3 +6,39 @@
 *			  AKA: CODED				*
  ***************************************
 */
+
+//ADMOB SETTINGS UNIVERSAL
+//initialize the goodies 
+function initAdNew(){
+        if ( window.plugins && window.plugins.AdMob ) {
+            var ad_units = {
+                ios : {
+                    banner: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
+                },
+                android : {
+                    banner: 'ca-app-pub-8484576454123628/6147471196',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-8484576454123628/9100937599'	//PUT ADMOB ADCODE HERE 
+                }
+            };
+            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+ 
+            window.plugins.AdMob.setOptions( {
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD 
+                bannerAtTop: false, // set to true, to put banner at top 
+                overlap: true, // banner will overlap webview 
+                offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
+                isTesting: false, // receiving test ad 
+                autoShow: true // auto show interstitial ad when loaded 
+            });
+ 
+            registerAdEvents();
+        } else {
+            //alert( 'admob plugin not ready' ); 
+        }
+}
+
+ initAdNew();
+showBannerFunc();
